67b0bbdac87933df24a3c7d70142389b1c6ce952
Sebastian Braende <sebastian.braende@gmail.com>
2023-08-23 15:58:32 +0200

    wip main, database and exterior_climate

--- exterior_climate.py
+++ exterior_climate.py
@@ -1,11 +1,13 @@
 import requests
 import utilities
+from datetime import datetime
 
 
 class OpenWeather():
     def __init__(self):
         self.config = utilities.HelperFunctions.open_config("config.json")
         self.url = self.make_url()
+        self.table_names = ["exterior_climate"]
 
     def make_url(self):
         openweather_url = "http://api.openweathermap.org/data/2.5/weather?"
@@ -18,17 +20,12 @@ class OpenWeather():
         try:
             response = requests.get(self.url).json()
             data = {
+                "datetime": datetime.now(), 
                 "temperature": round(utilities.HelperFunctions.kelvin_to_celsius
                                     (response["main"]["temp"]), 2),
                 "humidity": response["main"]["humidity"]
-                # "weather_main": response["weather"][0]["main"]
             }
         except requests.HTTPError as error:
             print(f"Http error {error}")
         return data
 
-
-class Conversion():
-    def kelvin_to_celsius(kelvin):
-        celsius = kelvin - 273.15
-        return celsius
