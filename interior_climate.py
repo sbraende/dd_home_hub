63e09b45ec7c58f32f2de54aa2ebfd67c1c6d543
Sebastian Braende <sebastian.braende@gmail.com>
2023-08-23 16:49:42 +0200

    Program can now log exterior and interior climate to db!

--- interior_climate.py
+++ interior_climate.py
@@ -1,19 +1,15 @@
+from datetime import datetime
 import requests
 import utilities
 
 
-class Config():
+class Setup():
     def __init__(self):
         self.config = utilities.HelperFunctions.open_config("config.json")
-        self.table_names = ["raspberry01", "raspberry02"]  ## For each raspbeery get raspberry name
+        # self.table_names = ["raspberry01", "raspberry02"]  ## For each raspbeery get raspberry name
+        self.raspberry_instances = self.get_raspberrys()
 
-    def get_interior_climate(self):
-        for raspberry in self.get_raspberrys():
-            data = self.get_url_data(raspberry.url)
-            # write data to database?
-            return data
-
-    def get_raspberrys(self) -> object:
+    def get_raspberrys(self) -> list:
         raspberry_instances = []
         raspberrys_in_json = self.config.get("interior_climate", {}).get("dd_home_server_api", {}).items()
         for raspberry_name, address_port in raspberrys_in_json:
@@ -21,13 +17,6 @@ class Config():
             raspberry_instances.append(raspberry_instance)
         return raspberry_instances
 
-    def get_url_data(self, url): 
-        response = requests.get(url)
-        if response.status_code == 200:
-            data = response.json()
-            return data
-        else:
-            print("Failed to retrieve data from API.")
 
 class Raspberry():
     def __init__(self, name, address_port) -> object:
@@ -35,4 +24,11 @@ class Raspberry():
         self.address_port = address_port
         self.url = f"http://{address_port}/data"
 
-        
+    def get_data(self): 
+        response = requests.get(self.url)
+        if response.status_code == 200:
+            data = response.json()
+            data.update({"datetime": str(datetime.now())})
+            return data
+        else:
+            print("Failed to retrieve data from API.")
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 import database
 import exterior_climate
 import interior_climate
@@ -6,23 +7,15 @@ import interior_climate
 class Database():
     def __init__(self) -> None:
         self.climate_database = database.Main("climate")
-        self.interior_instance = interior_climate.Config()
         self.exterior_instance = exterior_climate.OpenWeather()
-
-        self.climate_database.make_table(self.interior_instance.table_names[0],
-                                         self.interior_instance.get_interior_climate())
-        
-        self.climate_database.make_table(self.exterior_instance.table_names[0],
-                                         self.exterior_instance.get_weather())
+        self.interior_instance = interior_climate.Setup()
 
     def write_data(self):
-    #     self.climate_database.write_data("interior_climate", 
-    #                                      self.exterior_instance.get_weather())
-        self.climate_database.write_data(self.exterior_instance.table_names[0],  # Name of table
-                                         self.exterior_instance.get_weather())
-
-    def run(self):
-        pass
+        while True:
+            self.climate_database.write_data(self.exterior_instance.table_name,
+                                            self.exterior_instance.get_weather())
+            self.climate_database.write_interior_data(self.interior_instance.raspberry_instances)
+            time.sleep(120)  # Set to no lower than 120 seconds
 
 
 if __name__ == '__main__':
